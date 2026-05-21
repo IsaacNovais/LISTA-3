5b45fa99c19e50ca33fb382181b7b48bb0042ca2
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:35:06 -0300

    Add files via upload

---
+++ exercicio10/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int maior = int.MinValue, soma = 0;
+for (int i = 1; i <= 10; i++)
+{
+    int valor;
+    do
+    {
+        Console.Write($"Digite o {i}º valor positivo: ");
+        valor = int.Parse(Console.ReadLine());
+    } while (valor <= 0);
+    soma += valor;
+    if (valor > maior)
+    {
+        maior = valor;
+    }
+}
+Console.WriteLine($"O maior valor digitado é: {maior}");
+Console.WriteLine($"A soma dos valores digitados é: {soma}");
+Console.WriteLine($"A média aritmética dos valores digitados é: {soma / 10.0}");
+Console.ReadLine();
