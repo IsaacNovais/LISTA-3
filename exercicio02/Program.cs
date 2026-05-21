3bf407df818c6d7effa5e094de46ee4ce8ea9689
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:31:08 -0300

    Add files via upload

---
+++ exercicio02/Program.cs
@@ -0,0 +1,10 @@
+﻿int v2, v3;
+Console.Write("Digite o primeiro valor: ");
+v2 = int.Parse(Console.ReadLine());
+do
+{
+    Console.Write("Digite o segundo valor (maior que o primeiro): ");
+    v3 = int.Parse(Console.ReadLine());
+} while (v3 <= v2);
+Console.Write($"Os valores retornados são: {v2} e {v3}.");
+Console.ReadLine();
