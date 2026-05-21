0a55a0afdf7cd11f9f91b8b3c543e880af832ae9
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:31:43 -0300

    Add files via upload

---
+++ exercicio03/Program.cs
@@ -0,0 +1,8 @@
+﻿char sexo;
+do
+{
+    Console.Write("Digite o sexo (M/F): ");
+    sexo = char.Parse(Console.ReadLine().ToUpper());
+} while (sexo != 'M' && sexo != 'F');
+Console.Write($"O sexo retornado é: {sexo}.");
+Console.ReadLine();
