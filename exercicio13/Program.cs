e436e6136edf037c2d10e3b43c8d5fef05686f50
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:36:47 -0300

    Add files via upload

---
+++ exercicio13/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int fatorial;
+do
+{
+    Console.Write("Digite um número inteiro positivo: ");
+    fatorial = int.Parse(Console.ReadLine());
+} while (fatorial <= 0);
+
+for (int contadorFatorial = fatorial - 1; contadorFatorial >= 1; contadorFatorial--)
+{
+   fatorial *= contadorFatorial;
+}
+
+Console.WriteLine($"O fatorial é: {fatorial}");
+Console.WriteLine("Aperte qualquer tecla para continuar...");
+Console.ReadKey();
