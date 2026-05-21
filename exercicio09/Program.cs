60e405faa40fa79c13c61bb3ba4109376a8d4282
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:34:36 -0300

    Add files via upload

---
+++ exercicio09/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int fib1 = 1, fib2 = 1, fib3;
+Console.WriteLine($"Os primeiros trinta valores da série de Fibonacci são:");
+Console.WriteLine(fib1);
+Console.WriteLine(fib2);
+for (int i = 3; i <= 30; i++)
+{
+    fib3 = fib1 + fib2;
+    Console.WriteLine(fib3);
+    fib1 = fib2;
+    fib2 = fib3;
+}
+Console.ReadLine();
