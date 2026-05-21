250a2666f760b4600ed791363a7f34820ed3ff6e
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:33:38 -0300

    Add files via upload

---
+++ exercicio07/Program.cs
@@ -0,0 +1,12 @@
+﻿ 
+for (int i = 1; i <= 20; i++)
+{
+    Console.WriteLine($"Tabuada do {i}:");
+    for (int intervalo = 1; intervalo <= 10; intervalo++)
+    {
+        Console.WriteLine($"{i} x {intervalo} = {i * intervalo}");
+    }
+    Console.WriteLine("Pressione uma tecla para exibir a próxima tabuada...");
+    Console.ReadKey();
+    Console.Clear();
+}
