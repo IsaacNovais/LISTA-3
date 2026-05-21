7c503ed28751e3b5aa21d978a13f89076b8bdb41
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:32:42 -0300

    Add files via upload

---
+++ exercicio05/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int v5;
+do
+{
+    Console.WriteLine("Exibir a tabuada de um valor positivo digitado");
+    Console.Write("Digite um valor positivo: ");
+    v5 = int.Parse(Console.ReadLine());
+} while (v5 <= 0);
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{v5} x {i} = {v5 * i}");
+}
+Console.ReadLine();
