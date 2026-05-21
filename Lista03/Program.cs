b475753ab1ea9674c2a47726225587ba7ef5554c
IsaacNovais <isaac.novais@aluno.ifsp.edu.br>
2026-05-21 18:30:11 -0300

    Add files via upload

---
+++ Lista03/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int v1;
+do
+{
+    Console.Write("Digite um valor maior que zero: ");
+    v1 = int.Parse(Console.ReadLine());
+} while (v1 <= 0);
+Console.Write($"O valor retornado é: {v1}.");
+Console.ReadLine();
